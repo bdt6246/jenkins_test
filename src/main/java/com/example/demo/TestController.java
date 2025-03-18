f1478406d59dc283fcc557f94481fdaa0a026aff
bdt6246 <a01094079599@gmail.com>
2025-03-18 11:34:57 +0900

    [fix] Add get

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -1,11 +1,13 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/test")
 @RestController
 public class TestController {
+  @GetMapping("/test01")
   public String test(){
     System.out.println("test");
     return  "test";
